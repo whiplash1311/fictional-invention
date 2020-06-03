f93c993de56d499d00e2e324451712993eba73b7
whiplash1311 <48478371+whiplash1311@users.noreply.github.com>
2020-06-03 04:20:41 -0400

    Create NewDistroList.ps1

---
+++ NewDistroList.ps1
@@ -0,0 +1,1 @@
+new-distributiongroup -name 'Squad - Consumer MDM' -organizationalunit 'healthgrades.com/hg/groups' -samaccountname 'squad_consumermdm' -alias 'squad_consumermdm' -managedby will.mcbay
