5d83429c7cf34f9f2d5162d113cfcebc350b8e2d
whiplash1311 <48478371+whiplash1311@users.noreply.github.com>
2020-06-03 04:18:51 -0400

    Create BitLockerRecovery.ps1

---
+++ BitLockerRecovery.ps1
@@ -0,0 +1,3 @@
+$objComputer = Get-ADComputer ComputerName
+$Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword'
+$Bitlocker_Object
